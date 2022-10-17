189f0ba503a5c59142dcc50d8b377ae72743e3cf
adaatii <80161447+adaatii@users.noreply.github.com>
2022-10-17 20:34:31 -0300

    Update layout A_Event.

---
+++ Agenda_Pessoal/app/src/main/java/com/example/agenda_pessoal/View/A_NewEvent.java
@@ -0,0 +1,16 @@
+package com.example.agenda_pessoal.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agenda_pessoal.R;
+
+public class A_NewEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_event);
+    }
+}
